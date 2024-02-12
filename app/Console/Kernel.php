f973e9496bf3738f5b16a9ea5fd574695816c82a
Gerihh <horvathgergo1208@gmail.com>
2024-02-12 14:31:05 +0100

    cleanup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\Event;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +14,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Event::where(DB::raw("CONCAT(date, ' ', time)"), '<', now()->toDateTimeString())->delete();
+        })->everyThirtySeconds();
     }
 
     /**
