135855fe8b175cc194ef0a1da4fb5f94e0e727db
Gerihh <horvathgergo1208@gmail.com>
2024-02-20 09:40:27 +0100

    ...

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Event;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
+
 
 class EventController extends Controller
 {
